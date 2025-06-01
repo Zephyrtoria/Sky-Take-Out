ccf8c031360d0622099066a2c3bc7e76feb80696
Zephyrtoria <2594379136@qq.com>
2025-06-01 19:25:44 +0800

    feat: 购物车功能实现

--- sky-common/src/main/java/com/sky/constant/CacheConstant.java
+++ sky-common/src/main/java/com/sky/constant/CacheConstant.java
@@ -4,4 +4,5 @@ public class CacheConstant {
     public static final String SHOP_STATUS_KEY = "shop:status";
     public static final String DISH_CACHE_KEY = "dish:";
     public static final String SETMEAL_CACHE_NAME = "setmeal:cache";
+    public static final String SHOPPINGCART_CACHE_NAME = "shoppingcart:list";
 }
